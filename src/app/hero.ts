a69ed2b6eab8974fd91162d4d8e2dbce05e353a2
KMS06-25 <test@naver.com>
2019-01-12 16:57:58 +0900

    이니셜 커밋222

---
+++ src/app/hero.ts
@@ -0,0 +1,8 @@
+export class Hero {
+
+  constructor (public id:number , private name:string)
+  {
+
+  }
+
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,13 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero';
+
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent implements OnInit
+{
+  
+  //1)리터럴 객체로 객체 생성
 
-  constructor() { }
+  // hero:Hero = {
+  //   id : 11,
+  //   name : 'win'
+  // }
+
+  hero:Hero;
+
+  constructor() {
+    //2 ) new 키워드로 객체 생성
+    this.hero = new Hero();
+     this.hero.id = 222
+    // this.hero.name = "wind"
+    console.log(this.hero)
+
+  }
 
   ngOnInit() {
   }
