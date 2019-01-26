79cf1295a7d87a62c8a938d13eee59ec02ed6473
KMS06-25 <test@naver.com>
2019-01-26 10:27:23 +0900

    돔으로 리스트 뿌리기

--- src/app/jquery/jquery.component.ts
+++ src/app/jquery/jquery.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {HeroService} from '../hero.service';
+
+import * as $ from 'jquery';
+
 
 @Component({
   selector: 'app-jquery',
@@ -7,9 +11,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JqueryComponent implements OnInit {
 
-  constructor() { }
+  todoList;
+
+
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getTodoList();
   }
+
+  getTodoList() {
+    $.ajax({
+      url: 'http://www.javabrain.kr:8080/api/todo',
+      method: 'GET',
+      dataType: 'json',
+      success: data => {
+        console.log(data);
+        this.todoList = data;
+        this.refresh();
+
+        this.bbb = 1;
+      }
+    });
+  }
+
+
+  refresh() {
+    console.log('refresh');
+    $('#todo_list').empty();
+
+    this.todoList.forEach(function(item, index) {
+      let todo =
+        '<tr>' +
+        '<td>' +
+        (item.isFinished ? '완료' : '미완료') +
+        '</td>' +
+        (item.isFinished ? '<td style="text-decoration: line-through">' : '<td>') + item.todo + '</td>' +
+        '<td>' + item.created + '</td>' +
+        '<td>' + item.updated + '</td>' +
+        '<td>' +
+        '<button type="button">삭제</button>' +
+        '</td>' +
+        '</tr>';
+      $('#todo_list').append(todo);
+    });
+  }
+
 
 }
